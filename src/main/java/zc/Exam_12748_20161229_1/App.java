20448ca8de8b95e4d097dcb07fa532ea7e7b4ada
handzengcan <2849767983@qq.com>
2016-12-29 18:08:12 +0800

    考核项目

---
+++ src/main/java/zc/Exam_12748_20161229_1/App.java
@@ -0,0 +1,13 @@
+package zc.Exam_12748_20161229_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
